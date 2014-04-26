89a92fbaf2b07802c24aea6988208995804ca0c9
gapmeister11 <george@gr8baskets.com>
2014-04-26 02:11:02 -0400

    Added basic game functionality - it starts!

--- Everyone Is Bees/src/com/gapmeister/src/TheGame.java
+++ Everyone Is Bees/src/com/gapmeister/src/TheGame.java
@@ -1,5 +1,28 @@
 package com.gapmeister.src;
 
-public class TheGame {
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.AppGameContainer;
+
+public class TheGame extends StateBasedGame {
+
+	private static AppGameContainer window;
+	private static TheGame game;
+	
+	public static void main(String[] args) throws SlickException {
+		game = new TheGame();
+		window = new AppGameContainer(game, 800, 600, false);
+		game.init(window);
+		window.start();
+	}
+	
+	public TheGame() {
+		super("Uncle Lucy's Battle Royale ver. 1.048596");
+	}
+	
+	public void initStatesList(GameContainer window) throws SlickException {
+		
+	}
 	
 }

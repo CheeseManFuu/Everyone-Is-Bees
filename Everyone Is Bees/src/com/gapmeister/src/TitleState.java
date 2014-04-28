1b7ae8fb381c3ee3a08be3346d324c7d6a1e9e97
MokeymanQ <mokeymanq@gmail.com>
2014-04-28 01:18:10 -0400

    Reworked the intro movie.

--- Everyone Is Bees/src/com/gapmeister/src/TitleState.java
+++ Everyone Is Bees/src/com/gapmeister/src/TitleState.java
@@ -9,6 +9,11 @@ import org.newdawn.slick.state.StateBasedGame;
 public class TitleState extends BasicGameState {
 
 	private int milliCount = 0;
+	private int frameCount = 0;
+	
+	private String fullTitle = "Everyone     Is     Bees";
+	private String displayedTitle = "";
+	private final int FRAMES_PER_LETTER = 15;
 	
 	public void init(GameContainer window, StateBasedGame game) throws SlickException {//Called before the game runs
 
@@ -20,20 +25,32 @@ public class TitleState extends BasicGameState {
 	}
 
 	public void render(GameContainer window, StateBasedGame game, Graphics g) throws SlickException {//Render loop
-		g.drawString("Everyone", 100, 100);
+		g.drawString(displayedTitle, 100, 100);
+		/*g.drawString("Everyone", 100, 100);
 		if(milliCount >= 3000) {
 			g.drawString("Is", 200, 100);
 		}
 		if(milliCount >= 6000) {
 			g.drawString("Bees", 240, 100);
-		}
+		}*/
 	}
 
 	public void update(GameContainer window, StateBasedGame game, int delta) throws SlickException {//Game logic loop
-		milliCount += delta;
+		for(milliCount += delta; milliCount > 16; milliCount-= 16)
+		{
+			frameCount ++;
+			displayedTitle = fullTitle.substring(0, Math.min(fullTitle.length(), frameCount/FRAMES_PER_LETTER));
+			
+			//Waiting until the title has been drawn out, plus a bit of extra time.
+			if(frameCount >= FRAMES_PER_LETTER*(fullTitle.length()+10))
+			{
+				game.enterState(1);
+			}
+		}
+		/*
 		if(milliCount >= 10000) {
 			game.enterState(1);
-		}
+		}*/
 	}
 
 	public void exit(GameContainer window, StateBasedGame game) {//Called before exiting this state

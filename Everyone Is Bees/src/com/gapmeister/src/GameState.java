415ddb926725a8db81a0860c553a6785660c522d
MokeymanQ <mokeymanq@gmail.com>
2014-05-02 00:31:07 -0400

    Revert "Some shit works. Some doesn"
    This reverts commit 65e1ecd28854865816a5eb82b3ad67b06829b9a5.

--- Everyone Is Bees/src/com/gapmeister/src/GameState.java
+++ Everyone Is Bees/src/com/gapmeister/src/GameState.java
@@ -5,14 +5,14 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.geom.*;
+import org.newdawn.slick.geom.Circle;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class GameState extends BasicGameState {
 	
 	private MovementUnit ground;
-	private boolean up, down, left, right, stopped;
+	private boolean up, down, left, right;
 	private MovementVector playerMovement, mousePointer;
 	private Player p1;
 	private Weapon gun; //This should probably be moved later in development.
@@ -41,34 +41,16 @@ public class GameState extends BasicGameState {
 		g.setColor(Color.red);
 		g.draw(new Circle(mousePointer.x, mousePointer.y, 5));
 		hud.draw(g);
-		if(stopped)
-		{
-			/*
-		//WHY IS NOT RENDER
-		//I DO NOT UNDERSTAND
-			Color overlay = Color.black;
-			overlay.a = 0.8f;
-			g.setColor(overlay);
-			g.draw(new Rectangle(0, 0, 800, 600));*/
-			
-			g.setColor(Color.black);
-			g.fill(new Rectangle(window.getWidth()/2-35, window.getHeight()/2-10, 70, 20));
-			g.setColor(Color.white);
-			g.drawString("PAUSED", window.getWidth()/2-9*3, window.getHeight()/2-8);
-		}
 	}
 
 	public void update(GameContainer window, StateBasedGame game, int delta) throws SlickException {//Game logic loop
-		if(!stopped)
-		{
-			mousePointer = new MovementVector(window.getInput().getMouseX(), window.getInput().getMouseY());
-			playerMovement = new MovementVector((left?-1:0) + (right?1:0), (up?-1:0) + (down?1:0));
-			ground.update();
-			gun.update();
-			p1.moveNext(playerMovement);
-			p1.update();
-			hud.update();
-		}
+		mousePointer = new MovementVector(window.getInput().getMouseX(), window.getInput().getMouseY());
+		playerMovement = new MovementVector((left?-1:0) + (right?1:0), (up?-1:0) + (down?1:0));
+		ground.update();
+		gun.update();
+		p1.moveNext(playerMovement);
+		p1.update();
+		hud.update();
 	}
 	
 	public void keyPressed(int code, char key) {
@@ -83,8 +65,7 @@ public class GameState extends BasicGameState {
 			break;
 		}
 		if(code == Input.KEY_ESCAPE) {
-			container.setMouseGrabbed(stopped);
-			stopped = !stopped;
+			pause(true);
 		}
 	}
 	
@@ -104,6 +85,14 @@ public class GameState extends BasicGameState {
 	public void mouseReleased(int b, int x, int y)
 	{
 		gun.fire(p1.getPos(), mousePointer);
+	}
+	
+	void pause(boolean pause) {//TODO: Make this more functional
+		if(pause) {
+			container.setMouseGrabbed(false);
+		} else {
+			container.setMouseGrabbed(true);
+		}
 	}
 	
 	public void exit(GameContainer window, StateBasedGame game) {//Called before exiting this state
